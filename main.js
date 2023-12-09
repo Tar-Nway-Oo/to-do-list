97bc725dc830ed3cde9a6847089a7413c7694fe4
Tar-Nway-Oo <tarnwayo@gmail.com>
2023-12-09 15:02:43 +0630

    update removeList func and modify css

--- main.js
+++ main.js
@@ -3,12 +3,17 @@ const input = document.querySelector("#input");
 const addButton = document.querySelector("#add");
 const clearButton = document.querySelector("#clear");
 const listNumber = document.querySelector("#list-number");
+const warning = document.querySelector("#warning");
 
 let lists = [];
 
 addButton.addEventListener("click", () => {
   const inputValue = input.value;
-  if (inputValue === null || inputValue === "") return;
+  if (inputValue === null || inputValue === "") {
+     warning.textContent = "Please fill the input.";
+    return;
+  }
+  warning.textContent = "";
   const newList = {
     id: Date.now().toString(),
     task: inputValue,
@@ -23,21 +28,29 @@ clearButton.addEventListener("click", () => {
   render();
 });
 
-function markList(index) {
-  lists.splice(index, 1);
+function removeList(id) {
+  lists = lists.filter(li => li.id !== id);
   render();
 }
 
 function render() {
   clearList(listContainer);
   lists.forEach((list, index) => {
+    const listEl = document.createElement("div");
     const listItem = document.createElement("li");
+    const removeIcon = document.createElement("img");
+    
+    listEl.classList.add("list-item");
+    listEl.classList.add("text-bg-secondary");
     listItem.dataset.listId = list.id;
-    listItem.classList.add("list-item");
-    listItem.classList.add("border-bottom");
-    listItem.addEventListener("click", () => {markList(index)});
     listItem.innerText = list.task;
-    listContainer.appendChild(listItem);
+    removeIcon.setAttribute("src", "./images/remove-icon.png");
+    removeIcon.classList.add("remove-icon");
+    removeIcon.addEventListener("click", () => {removeList(list.id)});
+
+    listEl.appendChild(listItem);
+    listEl.appendChild(removeIcon);
+    listContainer.appendChild(listEl);
   });
   listNumber.textContent = lists.length;
 }
